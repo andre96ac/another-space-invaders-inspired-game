a9feffe56c8051c8370305a31994e4e817aa05f0
Andrea <andre96.ac@gmail.com>
2024-03-11 15:38:18 +0100

    fix build copy index

--- Utils/copy-index.js
+++ Utils/copy-index.js
@@ -1,10 +1,12 @@
 import * as fs from 'fs';
-
 import * as url from 'url';
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const __src = __dirname.replace("Utils", "src")
-const __dist = __dirname.replace("Utils", "dist")
 
-console.log(__src)
+console.log("Copying index.html in dist folter...")
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __src = __dirname.replace("utils", "src")
+const __dist = __dirname.replace("utils", "dist")
+
 
 fs.copyFileSync(`${__src}index.html`, `${__dist}index.html`);
+console.log("Index.html copied properly in dist folder!")
